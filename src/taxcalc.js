3bad4bfe9f15f41aab86826b6967e0b26e2f6b10
jiangweicheng <hifarer@qq.com>
2018-12-23 14:27:14 +0800

    个税计算器

--- src/taxcalc.js
+++ src/taxcalc.js
@@ -1,7 +1,7 @@
 
 // 个税起征点
 // const taxThreshold = 3500
-const taxThreshold = 5000
+const taxThreshold = 5000 + 1500    // 1500为租房专项扣除
 // 级距
 // const taxStep = [1500, 3000, 4500, 26000, 20000, 25000]
 const taxStep = [3000, 9000, 13000, 10000, 20000, 25000]
@@ -10,16 +10,18 @@ const taxRate = [0.03, 0.1, 0.2, 0.25, 0.3, 0.35, 0.45]
 
 /**
  * 个税计算器
- * @param {Number} netIncome 应发工资
- * @param {Number} ifRate 五险一金（实际为三险一金，工伤生育险个人不用出）占工资的比例，一般为22.5%
- * 社保基数15274.74 养老8% 医疗2% 失业0.5% 合计1603.85
- * 公积金基数19000 比例12% 合计2280
+ * 杭州2018年7月1日起社保封顶基数15274.74 养老8% 医疗2% 失业0.5%
+ * 杭州2018年7月1日起公积金封顶基数24311 比例12%
+ * http://www.hzgjj.gov.cn/art/2018/7/6/art_22_5344.html
+ * @param {Number} income 应发工资
+ * @param {Number} iRate 养老医疗失业险（工伤生育险个人不用出）缴存比例，一般为10.5%
+ * @param {Number} fRate 公积金（实际为三险一金，工伤生育险个人不用出）缴存比例，一般为12%
  */
-function taxCalc (netIncome, ifRate = 0.225) {
+function taxCalc (income, iRate, fRate) {
   // 五险一金合计
-  let insuranceFund = (1603.85 + 2280) || netIncome * ifRate
+  let insuranceFund = Math.min(15274.74, income) * iRate + Math.min(24311, income) * fRate
   // 应缴个税部分的工资金额
-  let taxNum = netIncome - insuranceFund - taxThreshold
+  let taxNum = income - insuranceFund - taxThreshold
   let temp = 0
 
   // 个税
@@ -40,11 +42,11 @@ function taxCalc (netIncome, ifRate = 0.225) {
 
   return {
     tax: Math.round(taxResult * 100) / 100,
-    income: Math.round((netIncome - insuranceFund - taxResult) * 100) / 100
+    income: Math.round((income - insuranceFund - taxResult) * 100) / 100
   }
 }
 
-let result = taxCalc(19000, 0.225)
+let result = taxCalc(19000, 0.105, 0.12)
 
 if (result.tax === 0) {
   console.log('不需要缴纳个税，实发工资：' + result.income)
